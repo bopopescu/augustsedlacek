249f0506c8f634eb3ae2c27a2b0cd50cdab99793
tomas44444 <tomas.psohlavec@aipberoun.cz>
2013-04-04 10:51:03 +0200

    Rotate images using 956__9 degrees info - v 0.1 (needs to update the js - put the code into single function!!)

--- var/www/js/bibedit_engine.js
+++ var/www/js/bibedit_engine.js
@@ -263,9 +263,243 @@ function setIventListeners() {
     image.src = $(this).attr('href');
     var w=600;
     var h=400;
-    newwindow = window.open($(this).attr('href'), 'window', 'width='+w+',height='+h+',top=100,left=500,scrollbars=1');
-    if (window.focus) {newwindow.focus()}
 
+var newwindow= window.open('', 'newwindow', 'width='+w+',height='+h+',top=100,left=500,scrollbars=1');
+if(newwindow.location == 'about:blank' ){
+    newwindow.location = $(this).attr('href');
+
+    newwindow.onresize=function(){
+        var angle = 0;
+if (this.document.getElementById('mycan')) {
+
+  var p =this.document.getElementById('mycan').oImage;
+
+  var n = p.src.indexOf('datastorage/');
+  var result = p.src.substring(n + 12);
+  var angleElm = $("td:contains("+result+")").parent().parent().find("td:contains('Zpusob natoceni')").next();
+  if (angleElm) angle = 1*angleElm.text();
+
+        //TP: see http://code.google.com/p/jquery-rotate/
+
+        p.angle = angle;
+
+        if (p.angle >= 0) {
+          var rotation = Math.PI * p.angle / 180;
+        } else {
+          var rotation = Math.PI * (360+p.angle) / 180;
+        }
+        var costheta = Math.cos(rotation);
+        var sintheta = Math.sin(rotation);
+
+        if (document.all && !window.opera) {
+          var canvas = this.document.getElementById('mycan');
+
+          canvas.src = p.src;
+          canvas.height = p.height;
+          canvas.width = p.width;
+
+          canvas.style.filter = "progid:DXImageTransform.Microsoft.Matrix(M11="+costheta+",M12="+(-sintheta)+",M21="+sintheta+",M22="+costheta+",SizingMethod='auto expand')";
+        } else {
+          var canvas = this.document.getElementById('mycan');
+          if (!p.oImage) {
+            canvas.oImage = new Image();
+            canvas.oImage.src = p.src;
+          } else {
+            canvas.oImage = p.oImage;
+          }
+
+          var orh=p.height;
+          var orw=p.width;
+
+          if (angle==90 || angle==270) {
+            canvas.oImage.height = this.window.innerWidth;
+            canvas.oImage.width = Math.abs(orh*this.window.innerWidth/orw);
+          } else {
+            canvas.oImage.width = this.window.innerWidth;
+            canvas.oImage.height = Math.abs(orh*this.window.innerWidth/orw);
+          }
+
+          canvas.style.width = canvas.width = Math.abs(costheta*canvas.oImage.width) + Math.abs(sintheta*canvas.oImage.height);
+          canvas.style.height = canvas.height = Math.abs(costheta*canvas.oImage.height) + Math.abs(sintheta*canvas.oImage.width);
+
+          var context = canvas.getContext('2d');
+          context.save();
+          if (rotation <= Math.PI/2) {
+            context.translate(sintheta*canvas.oImage.height,0);
+          } else if (rotation <= Math.PI) {
+            context.translate(canvas.width,-costheta*canvas.oImage.height);
+          } else if (rotation <= 1.5*Math.PI) {
+            context.translate(-costheta*canvas.oImage.width,canvas.height);
+          } else {
+            context.translate(0,-sintheta*canvas.oImage.width);
+          }
+          context.rotate(rotation);
+          context.drawImage(canvas.oImage, 0, 0, canvas.oImage.width, canvas.oImage.height);
+          context.restore();
+        }
+        canvas.id = "mycan";
+        canvas.angle = p.angle;
+
+//        p.parentNode.replaceChild(canvas, p);
+  }
+      }
+      newwindow.onload=function(){
+        var angle = 0;
+        var p =this.document.getElementsByTagName('img')[0];
+
+  var n = p.src.indexOf('datastorage/');
+  var result = p.src.substring(n + 12);
+  var angleElm = $("td:contains("+result+")").parent().parent().find("td:contains('Zpusob natoceni')").next();
+  if (angleElm) angle = 1*angleElm.text();
+
+  //      this.alert("angle: "+angle);
+
+        //TP: see http://code.google.com/p/jquery-rotate/
+        p.angle = angle;
+
+        if (p.angle >= 0) {
+          var rotation = Math.PI * p.angle / 180;
+        } else {
+          var rotation = Math.PI * (360+p.angle) / 180;
+        }
+        var costheta = Math.cos(rotation);
+        var sintheta = Math.sin(rotation);
+
+        if (document.all && !window.opera) {
+          var canvas = document.createElement('img');
+
+          canvas.src = p.src;
+          canvas.height = p.height;
+          canvas.width = p.width;
+
+          canvas.style.filter = "progid:DXImageTransform.Microsoft.Matrix(M11="+costheta+",M12="+(-sintheta)+",M21="+sintheta+",M22="+costheta+",SizingMethod='auto expand')";
+        } else {
+          var canvas = document.createElement('canvas');
+          if (!p.oImage) {
+            canvas.oImage = new Image();
+            canvas.oImage.src = p.src;
+          } else {
+            canvas.oImage = p.oImage;
+          }
+
+          var orh=p.height;
+          var orw=p.width;
+
+          if (angle==90 || angle==270) {
+            canvas.oImage.height = this.window.innerWidth;
+            canvas.oImage.width = Math.abs(orh*this.window.innerWidth/orw);
+          } else {
+            canvas.oImage.width = this.window.innerWidth;
+            canvas.oImage.height = Math.abs(orh*this.window.innerWidth/orw);
+          }
+
+          canvas.style.width = canvas.width = Math.abs(costheta*canvas.oImage.width) + Math.abs(sintheta*canvas.oImage.height);
+          canvas.style.height = canvas.height = Math.abs(costheta*canvas.oImage.height) + Math.abs(sintheta*canvas.oImage.width);
+
+          var context = canvas.getContext('2d');
+          context.save();
+          if (rotation <= Math.PI/2) {
+            context.translate(sintheta*canvas.oImage.height,0);
+          } else if (rotation <= Math.PI) {
+            context.translate(canvas.width,-costheta*canvas.oImage.height);
+          } else if (rotation <= 1.5*Math.PI) {
+            context.translate(-costheta*canvas.oImage.width,canvas.height);
+          } else {
+            context.translate(0,-sintheta*canvas.oImage.width);
+          }
+          context.rotate(rotation);
+          context.drawImage(canvas.oImage, 0, 0, canvas.oImage.width, canvas.oImage.height);
+          context.restore();
+        }
+        canvas.id = "mycan";
+        canvas.angle = p.angle;
+
+        p.parentNode.replaceChild(canvas, p);
+  //      alert(canvas.angle);
+      };
+
+    } else {
+ //     alert('je otevreno.. je tam '+newwindow.document.getElementById('mycan').oImage.src);
+      newwindow.document.getElementById('mycan').oImage.src=image.src;
+//      alert('je otevreno.. zmeneo na '+newwindow.document.getElementById('mycan').oImage.src);
+
+
+
+        var angle = 0;
+        var p =newwindow.document.getElementById('mycan').oImage;
+
+  var n = p.src.indexOf('datastorage/');
+  var result = p.src.substring(n + 12);
+  var angleElm = $("td:contains("+result+")").parent().parent().find("td:contains('Zpusob natoceni')").next();
+  if (angleElm) angle = 1*angleElm.text();
+
+        //TP: see http://code.google.com/p/jquery-rotate/
+
+        p.angle = angle;
+
+        if (p.angle >= 0) {
+          var rotation = Math.PI * p.angle / 180;
+        } else {
+          var rotation = Math.PI * (360+p.angle) / 180;
+        }
+        var costheta = Math.cos(rotation);
+        var sintheta = Math.sin(rotation);
+
+        if (document.all && !window.opera) {
+          var canvas = newwindow.document.getElementById('mycan');
+
+          canvas.src = p.src;
+          canvas.height = p.height;
+          canvas.width = p.width;
+
+          canvas.style.filter = "progid:DXImageTransform.Microsoft.Matrix(M11="+costheta+",M12="+(-sintheta)+",M21="+sintheta+",M22="+costheta+",SizingMethod='auto expand')";
+        } else {
+          var canvas = newwindow.document.getElementById('mycan');
+          if (!p.oImage) {
+            canvas.oImage = new Image();
+            canvas.oImage.src = p.src;
+          } else {
+            canvas.oImage = p.oImage;
+          }
+
+          var orh=p.height;
+          var orw=p.width;
+
+          if (angle==90 || angle==270) {
+            canvas.oImage.height = newwindow.window.innerWidth;
+            canvas.oImage.width = Math.abs(orh*newwindow.window.innerWidth/orw);
+          } else {
+            canvas.oImage.width = newwindow.window.innerWidth;
+            canvas.oImage.height = Math.abs(orh*newwindow.window.innerWidth/orw);
+          }
+
+          canvas.style.width = canvas.width = Math.abs(costheta*canvas.oImage.width) + Math.abs(sintheta*canvas.oImage.height);
+          canvas.style.height = canvas.height = Math.abs(costheta*canvas.oImage.height) + Math.abs(sintheta*canvas.oImage.width);
+
+          var context = canvas.getContext('2d');
+          context.save();
+          if (rotation <= Math.PI/2) {
+            context.translate(sintheta*canvas.oImage.height,0);
+          } else if (rotation <= Math.PI) {
+            context.translate(canvas.width,-costheta*canvas.oImage.height);
+          } else if (rotation <= 1.5*Math.PI) {
+            context.translate(-costheta*canvas.oImage.width,canvas.height);
+          } else {
+            context.translate(0,-sintheta*canvas.oImage.width);
+          }
+          context.rotate(rotation);
+          context.drawImage(canvas.oImage, 0, 0, canvas.oImage.width, canvas.oImage.height);
+          context.restore();
+        }
+        canvas.id = "mycan";
+        canvas.angle = p.angle;
+
+//        p.parentNode.replaceChild(canvas, p);
+
+}
+  
+
+    if (window.focus) {newwindow.focus()}
     if (!newwindow.closed) {newwindow.focus()}
     return false;   
   });
